1ba7ce44cf0b0049f715af0e4bbbefb9b11d516d
iavcoelho <iavcoelho@protonmail.com>
2022-12-28 17:41:54 +0000

    pesquisa

--- config.js
+++ config.js
@@ -3,7 +3,8 @@ tailwind.config = {
     extend: {
       colors: {
         primary: '#00AA95',
-        secondary: '',
+        secondary: '#A5E887',
+        light: '#D9FCC9',
         white: '#FFFFFF',
         accent : '#B7F49C',
         textAccent: '#00554b'
