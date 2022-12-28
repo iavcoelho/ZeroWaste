f6d5d78ce05772a084c709f063acb64ce6b0721e
iavcoelho <iavcoelho@protonmail.com>
2022-12-28 16:50:18 +0000

    start

---
+++ config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#00AA95',
+        secondary: '',
+        white: '#FFFFFF',
+        accent : '#B7F49C',
+        textAccent: '#00554b'
+      }
+    }
+  }
+}
